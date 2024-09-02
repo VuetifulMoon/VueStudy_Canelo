e066e1d91dedd829f31a78dbdf6d8999ae6cddc3
cyj1127 <157556973+cyj1127@users.noreply.github.com>
2024-09-02 17:36:10 +0900

    feat:페이지네이션 대충 완성

--- news/src/components/MainPage.vue
+++ news/src/components/MainPage.vue
@@ -11,6 +11,11 @@
       <option value="technology">technology</option>
     </select>
     <h1>뉴스 피드</h1>
+    <div v-for="page in totalPage" :key="page">
+      <button @click="pageNumber(page)">
+        {{ page }}
+      </button>
+    </div>
     <NewsItem />
   </div>
 </template>
@@ -21,15 +26,37 @@ export default {
   data() {
     return {
       category: "",
+      pageNum: 1,
+      totalPage: 1,
     };
   },
   created() {
-    this.$store.commit("getNews", "business");
+    //뉴스 가져오기
+    this.$store.commit("getNews", {
+      category: "business",
+      pageNum: 1,
+    });
+    //총 페이지 개수 저장
+    this.totalPage = this.$store.state.news.totalPage;
   },
+  beforeUpdate() {
+    this.$store.commit("getNews", {
+      category: "business",
+      pageNum: this.pageNum,
+    });
+  },
+
   methods: {
+    pageNumber(page) {
+      this.pageNum = page;
+      this.getNews();
+    },
     getNews() {
-      console.log(this.$store.state.category);
-      this.$store.commit("getNews", this.category);
+      this.$store.commit("getNews", {
+        category: this.category,
+        pageNum: this.pageNum,
+      });
+      this.totalPage = this.$store.state.news.totalPage;
     },
   },
 };
